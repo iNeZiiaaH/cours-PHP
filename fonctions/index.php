e8a160264a8754f6c059f5a0378dae1ee7eed27c
erwan didi <furiouse693@gmail.com>
2023-02-28 11:59:05 +0100

    changement de echo par var_dump

--- fonctions/index.php
+++ fonctions/index.php
@@ -6,21 +6,21 @@ function arrondi($nombre) {
     $decimal = $nombre - $entier;
     if ($decimal < 0.25) {
         return $entier;
-    } elseif ($decimal > 0.75) {
+    } elseif ($decimal >= 0.75) {
         return $entier + 1;
     } else {
         return $entier + 0.5;
     }
 };
 
-echo arrondi(0.234) . "<br />"; 
-echo arrondi(1.56) . "<br />"; 
-echo arrondi(1.27) . "<br />"; 
-echo arrondi(1.12) . "<br />"; 
-echo arrondi(1.25) . "<br />"; 
-echo arrondi(3.89) . "<br />"; 
-echo arrondi(7.23) . "<br />"; 
-echo arrondi(8.7) . "<br />"; 
-echo arrondi(2.45) . "<br />"; 
-echo arrondi(5.8) . "<br />"; 
-echo arrondi(10.2). "<br />"; 
+var_dump(arrondi(0.234)) . "<br />"; 
+var_dump(arrondi(1.56)) . "<br />"; 
+var_dump(arrondi(1.27)) . "<br />"; 
+var_dump(arrondi(1.12)) . "<br />"; 
+var_dump(arrondi(1.25)) . "<br />"; 
+var_dump(arrondi(3.89)) . "<br />"; 
+var_dump(arrondi(7.23)) . "<br />"; 
+var_dump(arrondi(8.7) ) . "<br />"; 
+var_dump(arrondi(2.45)) . "<br />"; 
+var_dump(arrondi(5.8)) . "<br />"; 
+var_dump(arrondi(10.2)) . "<br />"; 
